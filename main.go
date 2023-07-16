c1db43292b0f9affd7c127ef024964a674dba823
Siddharth Tewari <siddharthtewari14@gmail.com>
2023-07-16 14:52:10 +0530

    Fix: redis not needed rn

--- main.go
+++ main.go
@@ -4,19 +4,10 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/redis/go-redis/v9"
 	"github.com/sid-008/PubSub/broker"
 	"github.com/sid-008/PubSub/publish"
 	"github.com/sid-008/PubSub/subscribe"
 )
-
-func startServer() {
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-}
 
 func main() {
 	nodetype := os.Args[1]
